aff63bd3876ede6f0422ec86fd131de4dc255f43
shaiful00000 <130434006+shaiful00000@users.noreply.github.com>
2024-09-29 11:19:49 +0600

    App name changed

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Call XL',
+      title: 'Contacts-XL',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
